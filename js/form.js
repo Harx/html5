629c9ddb9e9fa35828c4ce1eac6d8a565e6dc5d4
harx <harx@live.cn>
2014-11-28 12:14:42 +0800

    update form(basic)

--- js/form.js
+++ js/form.js
@@ -1,8 +1,15 @@
-var provinces = ["", "江苏", "福建", "安徽"];
-var cities = [[], ["南京", "苏州", "南通"], ["福州",
+var provinces = ["请选择", "江苏", "福建", "安徽"];
+var cities = [["请选择"], ["南京", "苏州", "南通"], ["福州",
     "漳州", "泉州"], ["合肥", "蚌beng埠bu", "安庆"]];
 
 $(document).ready(function () {
+
+    /*--------ie类型检测*/
+    var isIE = !!window.ActiveXObject;
+    var isIE6 = isIE && !window.XMLHttpRequest;
+    var isIE8 = isIE && !!document.documentMode;
+    var isIE7 = isIE && !isIE6 && !isIE8;
+    
     /*----------------tools function */
     var addOptions = function (select, array, isClear) {
         if (!select) return false;
@@ -20,32 +27,50 @@ $(document).ready(function () {
             select.appendChild(option);
         }
     }
-
+    /*-------------form*/
+    var form=document.getElementById('f1');
+//    form.onsubmit=function(){return false;}
+    
     /*----------------------select */
     var select1 = document.getElementById('s1');
     var select2 = document.getElementById('s2');
     addOptions(select1, provinces);
     select1.selectedIndex = 0;
     addOptions(select2, cities[select1.selectedIndex]);
-    select1.addEventListener("change", function () {
-        addOptions(select2, cities[select1.selectedIndex]);
-    });
+    if (isIE7 || isIE8) {
+        select1.attachEvent("onchange", function () {
+            addOptions(select2, cities[select1.selectedIndex]);
+        })
+    } else {
+        select1.addEventListener("change", function () {
+            addOptions(select2, cities[select1.selectedIndex]);
+        });
+    }
 
-
-    var form = document.getElementById('f1');
+    /*-----------------------数字键盘*/
+    var keyNum = document.getElementById('keyNum');
+    var input = document.getElementById('tel');
     /*闭包实现保存变量*/
     for (var i = 0; i < 10; i++) {
-        var btn = document.createElement("button");
-        btn.innerHTML = i;
-
+        var btn = document.createElement("input");
+        btn.type="button";
+        btn.value = i;
+        btn.className="key-num";
         (function () {
-            var value = i;
-            btn.addEventListener("click", function (evt) {
-                evt.preventDefault();
-                alert(value);
-            });
+            var a = i;
+            if (isIE7 || isIE8) {
+                btn.attachEvent("onclick", function (evt) {
+                    if(input.value.length>=11) return false;
+                    input.value += a;
+                });
+            } else {
+                btn.addEventListener("click", function (evt) {
+                    if(input.value.length>=11) return false;
+                    input.value += a;
+                });
+            }
         }())
-        form.appendChild(btn);
+        keyNum.appendChild(btn);
     };
 
 });
