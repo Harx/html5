ce2fe5f1f47afcc5d3d10dd93d2f8b83b85676f2
harx <harx@live.cn>
2014-11-29 00:37:49 +0800

    add center.html

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-
 //------------Tool functions----------------
-String.prototype.capitalize=function(){
-    if(this==="") return this;
-    return this.charAt(0).toUpperCase()+this.slice(1);
+String.prototype.capitalize = function () {
+    if (this === "") return this;
+    return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
 
 //--------------List data---------------
 var arrItems = [
     "float",
-    "nav",//https://support.mozilla.org/zh-CN/?icn=tabz
+    "nav", //https://support.mozilla.org/zh-CN/?icn=tabz
     'form',
     'table',
     "slideshow",
     'slidesJs',
-    'jumbotron'//http://www.imooc.com/activity/zhaopin
+    'center',
+    'jumbotron' //http://www.imooc.com/activity/zhaopin
         ]
-//------------- add <a> to #list---------
+    //------------- add <a> to #list---------
 var list = document.getElementById('list');
 var i = 0,
     l = arrItems.length;
@@ -24,8 +24,6 @@ for (i; i < l; i++) {
     var id = arrItems[i];
     var a = document.createElement("a");
     a.href = "template/" + id + ".html";
-    a.innerHTML =id.capitalize();
+    a.innerHTML = id.capitalize();
     list.appendChild(a);
 }
-
-
