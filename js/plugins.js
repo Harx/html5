485398afc430dc1ff4c82b8b813121912f801e66
harx <harx@live.cn>
2014-12-18 15:40:52 +0800

    XHR,AddListener

--- js/plugins.js
+++ js/plugins.js
@@ -23,10 +23,96 @@
 
 // Place any jQuery/helper plugins in here.
 
-var addListener = function (element, type, handler) {
+/*----------------------------兼容性*/
+function addListener(element, type, handler) {
     if (element.addEventListener) {
         element.addEventListener(type, handler, false);
     } else if (element.attachEvent || document.all) {
         element.attachEvent("on" + type, handler);
     }
 }
+
+function createXHR() {
+    if (typeof XMLHttpRequest != "undefined") {
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != "undefined") {
+        if (typeof arguments.callee.activeXString != "string") {
+            //Microsoft.XMLHttp用于win8，2000系统。
+            var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.5.0", "MSXML2.XMLHttp.4.0", "MSXML2.XMLHttp.3.0", "MSXML2.XMLHttp", "Microsoft.XMLHttp"],
+                i, len;
+            for (i = 0, len = versions.length; i < l; i++) {
+                try {
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (ex) {
+
+                }
+            }
+        }
+        log(arguments.callee.activeXString);
+        return new ActiveXObject(arguments.callee.activeXString);
+    } else {
+        throw new Error(" No XHR object avaliable.");
+    }
+}
+
+function log(string) {
+    var log = document.getElementById("console");
+    if (!log) {
+        var log = document.createElement("div");
+        log.id = "console";
+        log.style.position = "absolute";
+        log.style.top = "0px";
+        log.style.right = "0px";
+        log.style.width = "300px";
+        log.style.height = "500px";
+        log.style.boder = "1px solid black";
+        log.style.background = "#dedede";
+        log.style.padding = "5px";
+        log.style.overflow="hidden";
+        document.body.appendChild(log);
+    }
+    log.innerHTML += "<p>" + string + "</p>";
+}
+/* 动画类--------------------------------------------*/
+/* 构造函数*/
+function Animate(config) {
+    this.target = config.target;
+    this.attribute = config.attribute;
+    this.startValue = config.startValue;
+    this.endValue = config.endValue;
+    this.effect = config.effect || "sin";
+    this.duration = config.duration || 300;
+    this.fps = config.fps || 60;
+    this.interval = 1000 / this.fps;
+
+    this.run();
+};
+/* 原型*/
+Animate.prototype = {
+    run: function () {
+        var startTime = new Date();
+        var self = this;
+        var timer = setInterval(function () {
+            var curTime = new Date();
+            var percent = (curTime - startTime) / self.duration;
+            if (percent > 1) {
+                self.target.style[self.attribute] = self.endValue + "px";
+                clearInterval(timer);
+            } else {
+                percent = Animate.effect[self.effect](percent);
+                self.target.style[self.attribute] = self.startValue + ((self.endValue - self.startValue) * percent) + "px";
+            }
+        }, this.interval);
+    }
+};
+/* 类方法*/
+Animate.effect = {
+    line: function (p) {
+        return p;
+    },
+    sin: function (percent) {
+        return Math.sin(Math.PI * 0.5 * percent);
+    }
+};
